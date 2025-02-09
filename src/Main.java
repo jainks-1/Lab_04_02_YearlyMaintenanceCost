bd5f0329950d68cc2dbb36b40de6bc4476a785f5
jainks-1 <jainks@mail.uc.edu>
2025-02-09 17:51:46 -0500

    Added purpose comment at the top

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+// Kaden Jain
+// Computer Programming 1 - Spring 2025
+/* Purpose: A program that uses home maintenance costs for each of
+the four seasons and returns the total yearly maintenance costs */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // declare variables
+        double springCost = 134.32;
+        double summerCost = 98.50;
+        double fallCost = 111.12;
+        double winterCost = 150.37;
+        double totalMaintenanceCost;
+
+        // logic
+        totalMaintenanceCost = springCost + summerCost + fallCost + winterCost;
+
+        // print output
+        System.out.println("The maintenance cost for Spring was: $" + springCost);
+        System.out.println("The maintenance cost for Summer was: $" + summerCost);
+        System.out.println("The maintenance cost for Fall was: $" + fallCost);
+        System.out.println("The maintenance cost for Winter was: $" + winterCost);
+        System.out.println("The total maintenance cost for the year was: $" + totalMaintenanceCost);
+
     }
 }
